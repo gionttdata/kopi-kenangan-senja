3dd0d300a57cfbce468cf25dff48e9b7794846a7
Giovanni PC Kantor <giovanni.nttdata@gmail.com>
2023-11-09 17:51:01 +0700

    Adiitonal search-form

--- js/script.js
+++ js/script.js
@@ -19,11 +19,15 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
-// Click outside sidebar to hide Navbar (if screensize < tablet)
-const hamburger = document.querySelector("#hamburger-menu");
+// Klik di luar element
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
